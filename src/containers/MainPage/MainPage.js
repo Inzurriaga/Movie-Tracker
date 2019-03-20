0608c7555948726fee322d767694af7aefdf4ec5
Gabriel Inzurriaga <gabeinzurriaga@yahoo.com>
2019-03-19 18:19:45 -0600

    slide show container created

--- src/containers/MainPage/MainPage.js
+++ src/containers/MainPage/MainPage.js
@@ -1,16 +1,11 @@
 import React, { Component } from 'react';
-
+import { fetchMovies } from "../../api/index"
 import { connect } from 'react-redux';
 import { getMovies } from '../../actions';
+import SlideShow from "../SlideShow/SlideShow"
 
 
 class MainPage extends Component {
-  constructor(){
-    super()
-
-  }
-  // api key 3033b069b2d0b32039c7d85cfe10cfd3
-
   // Genres
   // https://api.themoviedb.org/3/genre/movie/list?language=en-US&api_key=3033b069b2d0b32039c7d85cfe10cfd3
 
@@ -21,12 +16,14 @@ class MainPage extends Component {
   // http://image.tmdb.org/t/p/original/{Extension}
 
   // Discover
-  componentDidMount() {
-    const url = "https:api.themoviedb.org/3/discover/movie?page=1&include_video=true&include_adult=false&sort_by=popularity.desc&language=en-US&api_key=3033b069b2d0b32039c7d85cfe10cfd3";
-    fetch(url)
-      .then(response => response.json())
-      .then(movies => this.props.getMovies( movies.results ))
-      .catch(error => error.message)
+  componentDidMount = async () => {
+    const urlDiscover = "https:api.themoviedb.org/3/discover/movie?page=1&include_video=false&include_adult=false&sort_by=popularity.desc&language=en-US&api_key=";
+    try {
+      const response = await fetchMovies(urlDiscover, "Discover")
+      this.props.getMovies( response.results )
+    } catch(error) {
+      console.log(error.message)
+    }
   }
 
   render() {
@@ -46,7 +43,14 @@ class MainPage extends Component {
     console.log('movies', movies);
 
     return (
-      <div className="MainPage">{movieResults}</div>
+      <div className="MainPage">
+        {
+          this.props.movies.length ?
+          <SlideShow /> : 
+          <div>im loading</div>
+        }
+        {movieResults}
+      </div>
     );
   }
 }
